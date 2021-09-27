c65e572df3d831fb87b19f669aa883928aa601a7
Alisson Braga <alisson.braga.2019@gmail.com>
2021-09-27 19:25:38 -0300

    Precificação

---
+++ src/precificacao/Produto.java
@@ -0,0 +1,15 @@
+package precificacao;
+
+public class Produto {
+	public String nome;
+	public float precoDeCusto;
+	public byte quantidade; 
+	
+	public float calcularPrecoDeVenda(float custoFixo, float lucroLiquido) {
+		float totalDaCompra = this.precoDeCusto * this.quantidade;
+		float faturamento = custoFixo + totalDaCompra + lucroLiquido;
+		return faturamento / this.quantidade;	
+
+	}
+}	
+
